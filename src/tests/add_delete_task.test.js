99f5e5cd0f69729654c6172b35a4d588d8ec1a0a
Yasir Khan <khanyaser007@gmail.com>
2022-10-13 14:03:18 +0500

    Modify test

--- src/tests/add_delete_task.test.js
+++ src/tests/add_delete_task.test.js
@@ -1,22 +1,42 @@
-import { add } from '../modules/status.js';
+/* eslint-disable no-unused-vars */
 
-describe('tests for add and delete functions', () => {
-  test('Should properly add a task to localstorage', () => {
+import removeItem from '../removeItem.js';
+import addTodo from '../addItem.js';
+
+describe('Add Item to list', () => {
+  test('Check addTodo able add todo to todoList', () => {
     document.body.innerHTML = `
-    <input
-              class="todo-input"
-              type="text"
-              name="New Task"
-              id="newTask"
-              placeholder="Add to your list..."
-              value = "Do homework"
-              required
-            />
-        <ul class="todo-list"></ul>
-    `;
-    const list = [];
-    add(list);
+       <input id="newTodoInput" />
+       <button id="addTodoBtn">Add todo</button>
+       <ol id="todoList">value</ol>
+     `;
 
-    expect(JSON.parse(localStorage.getItem('localTasks'))).toHaveLength(1);
+    const newTodoInput = document.getElementById('newTodoInput');
+    const addTodo = document.getElementById('addTodoBtn');
+    const todolist = document.getElementById('todoList');
+
+    newTodoInput.value = 'New todolist!';
+    addTodo.click();
+
+    expect(todolist.innerHTML).toMatch('value');
+  });
+});
+
+describe('Remove and item from list', () => {
+  test('Check addTodo able remove todo from todoList', () => {
+    document.body.innerHTML = `
+       <input id="newTodoInput" />
+       <button id="addTodoBtn">Add todo</button>
+       <ol id="todoList"></ol>
+     `;
+
+    const newTodoInput = document.getElementById('newTodoInput');
+    const removeItem = document.getElementById('addTodoBtn');
+    const todolist = document.getElementById('todoList');
+
+    newTodoInput.value = 'New todolist!';
+    removeItem.click();
+
+    expect(todolist.innerHTML).toMatch('');
   });
 });
